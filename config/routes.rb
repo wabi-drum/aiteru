87b1ea1238602bdc823b35b4f2d75f15f4a02c12
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-05-05 12:45:16 +0000

    aiteru 5/5/21:45

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
