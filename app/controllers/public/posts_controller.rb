2b5e836e4ae9d9a000a2daf41d144d7aaa5c257d
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-05-20 11:17:44 +0000

    aiteru5/20/20:17

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -29,7 +29,7 @@ class Public::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image)
   end
 
 end
