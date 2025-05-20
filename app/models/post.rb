f82cb96d3ed359f71be1a1330fb35f014fe2549f
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-05-20 13:23:19 +0000

    aiteru5/20/22:23

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,12 @@ class Post < ApplicationRecord
   validates :body, presence: true
   validates :image, presence: true 
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(gravity: :center, resize: "#{width}x#{height}^", crop: "#{width}x#{height}+0+0")
   end
   
 end
