053526a4676325e757a3dbe29e27a0397382d816
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-05-24 10:09:15 +0000

    aiteru5/24/19:09

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,9 +8,21 @@ class Public::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to new_user_session_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 
 end
