36ec3c702c2b371643dc241aa711cab0414d634d
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-09-19 12:34:34 +0000

    edited plan/show

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -7,4 +7,15 @@ class Schedule < ApplicationRecord
 
   validates :date_time, presence: true
   validates :status, presence: true 
+  validate :date_within_plan_period
+
+  private
+
+  def date_within_plan_period
+    return if date_time.blank?
+
+    if date_time.to_date < plan.starting_date || date_time.to_date > plan.ending_date
+      error.add(:date_time, "Must be within the plan period.")
+    end
+  end
 end
