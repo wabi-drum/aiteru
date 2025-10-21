87fc54caca59f912c59f95f498f123c1c21bc6f8
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-10-21 11:32:17 +0000

    like's feature WIP view almost done

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -7,7 +7,7 @@ class Public::LikesController < ApplicationController
     redirect_to post_path(post)
   end
 
-  def deatroy
+  def destroy
     post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: post.id)
     like.destroy
